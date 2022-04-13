8bf18ab7a8eac37fe0bb2aac15f0b1c74892beb2
AnyamahNdidi <theo4felix@gmail.com>
2022-04-13 18:33:20 +0100

    another change

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+require('dotenv').config()
+require("./config/db.js")
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 5040
+const myRouter = require("./Router/router")
+const cors = require("cors")
+const port = process.env.PORT || 2022
 
 app.get("/", (req, res)=>{
    res.send("port is running well well")
 })
+
+
+app.use(express.json())
+app.use(cors())
+app.use("/api", myRouter)
 
 app.listen(port, ()=>{
   console.log( `port is running on ${port}`);
